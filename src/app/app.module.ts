c519d04ec4e9c7a34ba5daa047a5a306f45ed7da
wassim <wassim.bentaala@gmail.com>
2018-11-03 19:41:03 +0100

    Add Create Employee fonctionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { ListeEmployeesComponent } from './employees/liste-employees.component';
@@ -20,7 +22,9 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule, 
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BsDatepickerModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
-  constructor() { }
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  gender = 'male';
+  contact = 'email';
+  isActive = true;
+  department = '3';
+  dateOfBirth: Date = new Date();
+  departments: Department[] = [
+    {id: 1, name: "Help Desk"},
+    {id: 2, name: "HR"},
+    {id: 3, name: "IT"},
+    {id: 4, name: "Paroll"}
+  ];
+  constructor() { 
+    this.datePickerConfig = Object.assign({}, 
+      {
+        containerClass: 'theme-dark-blue', 
+        showWeekNumbers: false 
+      });
+  }
 
   ngOnInit() {
+  }
+
+  saveEmployee(employee: NgForm): void{
+    console.log(employee.value);
   }
 
 }
